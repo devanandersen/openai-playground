e12e881edba8e4b5ca502a76726b149e426d5d4e
Devan Andersen <devan.andersen@shopify.com>
2023-07-11 18:55:16 -0400

    adjust

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from auth import *
 
-def execute():
+def gpt():
     prompt = "Translate the following English text to French: 'Hello, world!'"
 
-    # Get a response from the API
-    response = openai.Completion.create(
-      engine="text-davinci-003",
-      prompt=prompt,
-      max_tokens=60
+    openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Who won the world series in 2020?"},
+            {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+            {"role": "user", "content": "Where was it played?"}
+        ]
     )
 
     # Print the response
     print(response.choices[0].text.strip())
 
 set_openai_key()
-execute()
+gpt()
